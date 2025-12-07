e7c63001006c91b87467d42615cff8cd772f4d17
william68233cs-spec <william68233cs@gmail.com>
2025-12-07 22:23:13 +0000

    join.js

--- join.js
+++ join.js
@@ -1,20 +1,28 @@
 const socket = io();
-const boxes = document.querySelectorAll(".code-box");
+const inputs = document.querySelectorAll('.code-box');
 
-boxes.forEach((box, index) => {
-    box.addEventListener("input", () => {
-        if (box.value && index < boxes.length - 1) {
-            boxes[index + 1].focus();
+inputs.forEach((input, index) => {
+    input.addEventListener('input', (e) => {
+        if (input.value.length === 1 && index < inputs.length - 1) {
+            inputs[index + 1].focus();
+        }
+    });
+
+    input.addEventListener('keydown', (e) => {
+        if (e.key === 'Backspace' && input.value === '' && index > 0) {
+            inputs[index - 1].focus();
         }
     });
 });
 
-document.getElementById("joinGameBtn").addEventListener("click", () => {
+document.getElementById("joinGameBtn").addEventListener("click", function () {
     let code = "";
-    boxes.forEach(b => code += b.value);
+    inputs.forEach(input => {
+        code += input.value;
+    });
 
     if (code.length < 6) {
-        alert("Please enter a full 6-digit code.");
+        alert("Please enter the full 6-digit code.");
         return;
     }
 
@@ -22,10 +30,11 @@ document.getElementById("joinGameBtn").addEventListener("click", () => {
 });
 
 socket.on('join_success', function(data) {
-    alert("Success! You joined room: " + data.room);
+    window.location.href = "/game?room=" + data.room;
 });
 
 socket.on('error_msg', function(data) {
-    alert("Error: " + data.msg);
+    alert(data.msg);
+    inputs.forEach(input => input.value = '');
+    inputs[0].focus();
 });
-
